c3e1786782080165e7c2a873ad86c6780344e21e
Hamza Kazi <hamzakaziii1@gmail.com>
2022-08-24 12:54:13 +0100

    YELP

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :restaurants
+
+  get "restaurants", to: "restaurants#index"
+
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant_show
+
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+
+  delete "restaurants/:id", to: "restaurants#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5, chef_name: "John"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4, chef_name: "Thomas"}
+hoxton101 =  {name: "hoxton101", address: "73 High Road, London E15 9LW", rating: 2, chef_name: "Mike"}
+kfc =  {name: "KFC", address: "221b Baker Street, London N20 36H", rating: 5, chef_name: "Sherlock"}
+pizza_hut =  {name: "pizza_hut", address: "87 Lamberth Street, London E20 8US", rating: 3, chef_name: "Marie"}
+
+# [dishoom, pizza_east].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+puts "Finished!"
